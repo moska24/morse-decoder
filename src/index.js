e6bc80ce7e011650593dbe97d392487944b95dd8
Natalia <lazorenko.natasha@yandex.ru>
2023-10-12 16:17:04 +0800

    updated file src/index.js

--- src/index.js
+++ src/index.js
@@ -38,8 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let symbols = [];
+    let masResult = '';
+
+    for (let i = 0; i < expr.length; i = i + 10){
+        let symbol = expr.slice(i, i + 10);
+        symbols.push(symbol);
+    }
+    console.log(symbols);
+    for (let i = 0; i < symbols.length; i++){
+        if (symbols[i] === '**********'){
+            masResult  = masResult + ' ';
+        }
+        else masResult = masResult + morseDecoder(symbols[i]);
+    }
+   console.log(masResult);
+   return masResult;
 }
+
+function morseDecoder(symbol) {
+    let strMorse = '';
+    console.log(symbol);
+    for (let i = 0; i < symbol.length; i = i + 2){
+        let combination = symbol.slice(i, i + 2);
+        console.log(combination);
+        if (combination === '10') {
+            strMorse = strMorse + '.';
+            console.log(strMorse);
+        }
+        else if (combination === '00') {
+            console.log('here');
+            strMorse = strMorse + '';
+        }
+        else if (combination === '11') {
+            strMorse = strMorse + '-';
+        }
+    }
+    return MORSE_TABLE[strMorse];
+}
+
 
 module.exports = {
     decode
